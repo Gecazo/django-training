6ec49916e18b498dbe570adad14f622947383640
Filip <43730405+Gecazo@users.noreply.github.com>
2021-09-06 12:37:24 +0300

    Defining model

---
+++ learning_logs/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Topic(models.Model):
+    """Topic to be explored by user"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns text representation of the model"""
+        return self.text
+    
